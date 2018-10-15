e01d21980aff86a0b33c2b0e558f20ca22754cec
woznima9 <37245064+woznima9@users.noreply.github.com>
2018-10-15 11:05:27 +0200

    przygotowane arkusze jsp: formularz do zbierania danych i strona zapisu do bazy. Do zrobienia ładowanie danych do bazy mySQL-checking

--- src/main/java/pl/sdacademy/DodajServlet.java
+++ src/main/java/pl/sdacademy/DodajServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 // 20180707 so
+// checking 2018-10-15
 public class DodajServlet extends HttpServlet {
 
 
